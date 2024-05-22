51fcf45659c7133035e7ed603dbd0a79e29d19b0
0xJs <jpsaad00@gmail.com>
2024-05-22 18:15:21 -0400

    Testing approaches

---
+++ add-to-calendar/add_to_calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  add_to_calendarApp.swift
+//  add-to-calendar
+//
+//  Created by 0xJs on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct add_to_calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ add-to-calendarUITests/add_to_calendarUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  add_to_calendarUITestsLaunchTests.swift
+//  add-to-calendarUITests
+//
+//  Created by 0xJs on 5/22/24.
+//
+
+import XCTest
+
+final class add_to_calendarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
